acb46faa103c159c804ee0e9e169b2bc02403212
Rich Steinmetz <richard.had@hotmail.de>
2021-11-11 00:05:46 +0100

    Complete differences of squares

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,27 +1,19 @@
 class Squares
-  attr_reader :input_arr
+  attr_reader :n
 
-  def initialize(input)
-    @input_arr = [*1..input]
+  def initialize(n)
+    @n = n
   end
 
   def square_of_sum
-    input_arr.reduce(0, :+) ** 2
+    ((n / 2.0) * (1 + n)) ** 2
   end
 
   def sum_of_squares
-    input_arr.map(&:**.with(2)).reduce(:+)
+    ((2 * n + 1) * n * (n + 1)) / 6
   end
 
   def difference
     square_of_sum - sum_of_squares
-  end
-end
-
-# patch Symbol ":" to use map like this: map(&:**.with(2))
-# https://stackoverflow.com/a/23711606/5925094
-class Symbol
-  def with(*args, &block)
-    ->(caller, *rest) { caller.send(self, *rest, *args, &block) }
   end
 end
